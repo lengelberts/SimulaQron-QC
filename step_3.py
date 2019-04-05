fc5950ac3a7002b67eb08b3d01b8c8f8ee767b59
Lynn Engelberts <lynn.engelberts@student.auc.nl>
2019-04-05 19:08:54 +0200

    step_3_Alice and step_3_Bob to be runned in different terminals

--- step_3.py
+++ step_3.py
@@ -1,10 +1,15 @@
 from cqc.pythonLib import CQCConnection, qubit
 
-def step_3(theta_A):
+
+def step_3_Alice(theta_A):
     """Alice sends her basis information theta_A to Bob.""" # Classical information
     with CQCConnection("Alice") as Alice:
         Alice.sendClassical("Bob",theta_A)
+        print("Bob has received the classical information.") # Can also do without indent
 
+
+def step_3_Bob():
+    """Bob receives the basis information from Alice. Return basis information as a list."""
     with CQCConnection("Bob") as Bob:
         theta = Bob.recvClassical()
-        print(list(theta))
+        return list(theta)
